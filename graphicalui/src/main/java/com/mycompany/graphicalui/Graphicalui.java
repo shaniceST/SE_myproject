514d12c14dde1202214e1687672c99e49d0e902d
shaniceST <shanice.gathogo@strathmore.edu>
2025-12-03 20:23:07 +0300

    Add all Netbeans projects: CarManagementSystem, Cohesion, Coupling, FunctionalProgramming, graphical UI projects

---
+++ graphicalui/src/main/java/com/mycompany/graphicalui/Graphicalui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.graphicalui;
+
+/**
+ *
+ * @author shani
+ */
+public class Graphicalui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ graphicaluserinterface/src/main/java/com/mycompany/graphicaluserinterface/Graphicaluserinterface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.graphicaluserinterface;
+
+/**
+ *
+ * @author shani
+ */
+public class Graphicaluserinterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
